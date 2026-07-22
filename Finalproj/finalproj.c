3d612f53540ae9f7a44468fa8b31420d29fdc611
Pmorlando <pmorlando67@gmail.com>
2026-07-22 15:50:30 -0600

    now triggering proper but need to add some cooldown so it doesnt go for 3 or 4 frames

--- Finalproj/finalproj.c
+++ Finalproj/finalproj.c
@@ -57,7 +57,7 @@ static int firstframe = 1;
 static double diffhistory[5] = {0};// wil lsee if 5 is enough
 static int diffidx = 0;
 static int diffhistcount = 0;
-static int spikethresh = 0.42;// test if this is an accurate thresh and increase if needed
+static double spikethresh = 0.42;// test if this is an accurate thresh and increase if needed
 
 //static double frametotal = 0; idk if ill need 
 
@@ -869,7 +869,7 @@ static int spikethensettle(void) // if frame after diff calc spike
 			break;
 		}
     }
-    if(spike > 1) return 1; // 1 if there is good frame taking out  && lastdiff < spikethresh for testing
+    if(spike == 1 && lastdiff < spikethresh) return 1; // 1 if there is good frame taking out   for testing
     return 0; // 0 if fail test
 }
 
